c6c1a625948b8704d457291021c6e28da998cfc7
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2021-12-08 12:19:59 +0100

    Fix js after jquery removal

--- public/resources/assets/scripts/main.js
+++ public/resources/assets/scripts/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', (e) => {
-	if (window.scrollTop + window.innerHeight > document.innerHeight - 100) {
-		document.getElementById("akyCookiesGestion").classList.add("active");
-	} else {
-		document.getElementById("akyCookiesGestion").classList.remove("active");
-	}
+    if (window.scrollY + window.innerHeight > document.documentElement.scrollHeight - 100) {
+        document.getElementById("akyCookiesGestion").classList.add("active");
+    } else {
+        document.getElementById("akyCookiesGestion").classList.remove("active");
+    }
 });
